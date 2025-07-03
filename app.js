79723a5c09924045cc9e305fd51b3cee6c99f0fe
Manav solanki <manavsolanki344@gmail.com>
2025-07-03 23:57:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+const menu = document.querySelector("#menu");
+const navbar = document.querySelector(".Navbar");
 const header = document.querySelector("header");
 
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",window.scrollY>60)
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 60)
 })
+
+menu.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
+
+function closeNavbar() {
+    if (window.innerWidth <= 768) {
+        navbar.classList.remove("active");
+    }
+}
